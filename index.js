f6619b082b0844edae44ce38bfe6e5da3d5a7634
t-tunmoonsuk <tonnow6@gmail.com>
2025-02-11 22:00:44 +0700

    zzz

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', productRoutes);
+
 
 // เชื่อมต่อ MongoDB
 mongoose.connect(process.env.MONGO_URI, {
